e7d27447603b7a4f571b467cf15482ad222b3b69
Pokmon <104436135+PokmonArchi@users.noreply.github.com>
2023-04-12 17:10:56 +0200

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$nick = $_POST['nick'];
-$botName = $_POST['bot-nazwa'];
-$info = $_POST['uwagi'];
-$rodzajzlecenia = $_POST['select'];
-$message = `Nick użytkownika: $nick\nRodzaj Zlecenia: $rodzajzlecenia\nNazwa bota: $botName\nUwagi:\n$info`;
-mail("contact.pokmonbot@gmail.com", "Zlecenie na bota", $message);
-echo "Udało się wysłać maila!";
-?>
