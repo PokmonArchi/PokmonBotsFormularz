cadccad1ca7deb8489c5198d4bb2702c5609c474
Pokmon <104436135+PokmonArchi@users.noreply.github.com>
2023-04-12 17:40:26 +0200

    Add files via upload

--- wyslijzlecenie.js
+++ wyslijzlecenie.js
@@ -1,17 +1,26 @@
-function sendMail(){
-    let nick = document.getElementById("nick").value;
-    let botName = document.getElementById('bot').value;
-    let rodzajzlecenia = document.getElementById('select').value;
-    let uwagi = document.getElementById('uwagi').value;
-    let message = `Nick użytkownika: ${nick}\n Nazwa bota: ${botName}\n rodzaj zlecenia: ${rodzajzlecenia}\n Uwagi: ${uwagi}`;
-    Email.send({
-        Host: "smtp.mailtrap.io",
-        Username: "a9ffe2f7e594d3",
-        Password: "fec72bf71c142c",
-        To:"to@example.com",
-        From:`from@example.com`,
-        Subject:"Zlecenie",
-        Body: `${message}`
-    }).then(alert("Udało się wysłać wiadomość!"));
-    console.log(Email);
-}
+var nodemailer = require('nodemailer');
+let nick = document.getElementById("nick").value;
+let botName = document.getElementById('bot').value;
+let rodzajzlecenia = document.getElementById('select').value;
+let uwagi = document.getElementById('uwagi').value;
+let message = `Nick użytkownika: ${nick}\n Nazwa bota: ${botName}\n rodzaj zlecenia: ${rodzajzlecenia}\n Uwagi: ${uwagi}`;
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'contact.pokmonbot@gmail.com',
+      pass: 'Poliki999'
+    }
+  });
+  var mailOptions = {
+    from: 'example@example.com',
+    to: 'contact.pokmonbot@gmail.com',
+    subject: 'Zlecenie',
+    text: `${message}`
+  };
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
